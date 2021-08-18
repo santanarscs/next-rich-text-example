bdf24076cf712b0ec72106b00e16d8f1b8abc416
santanarscs <raphaelstn@gmail.com>
2021-08-18 18:56:09 -0300

    add output json draftjs

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import { useState} from 'react'
 import { EditorState, convertFromRaw, convertToRaw } from "draft-js";
 import dynamic from 'next/dynamic'; 
+import tablePlugin from 'draft-js-table-plugin'
 const Editor = dynamic(
   () => import('react-draft-wysiwyg').then(mod => mod.Editor),
   { ssr: false }
@@ -22,15 +23,22 @@ const emptyContentState = convertFromRaw({
 });
 
 
+
+
 export default function Home() {
   const [editorState, setEditorState] = useState(
     () => EditorState.createWithContent(emptyContentState)
   )
+  const [formattedData, setFormattedData] = useState('')
   const onEditorStateChange = (editorState) => { setEditorState(editorState) }
   // const onEditorStateChange = (editorState) => {
   //   setEditorState(convertToRaw(editorState.getCurrentContent()))
   // }
   const uploadImageCallBack = () => {console.log('upload image')}
+
+  const handleSubmit = () => {
+    setFormattedData(convertToRaw(editorState.getCurrentContent()))
+  }
 
   return (
     <div className="bg-gray-300 min-h-screen flex flex-col items-center  justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -42,23 +50,38 @@ export default function Home() {
           toolbarClassName="toolbar-class"
           wrapperClassName="wrapper-class"
           editorClassName="editor-class"
-          toolbar={{
-            options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'history'],
-            inline: { inDropdown: true },
-            list: { inDropdown: true },
-            textAlign: { inDropdown: true },
-            link: { inDropdown: true },
-            history: { inDropdown: true },
-            image: { 
-              urlEnabled: true,
-              uploadEnabled: true,
-              uploadCallback: uploadImageCallBack, 
-              previewImage: true,
-              alt: { present: false, mandatory: false } 
-            },
-          }}
+          plugins={[tablePlugin]}
+          // toolbar={{
+          //   options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'history'],
+          //   inline: { inDropdown: true },
+          //   list: { inDropdown: true },
+          //   textAlign: { inDropdown: true },
+          //   link: { inDropdown: true },
+          //   history: { inDropdown: true },
+          //   tablePlugin,
+          //   image: { 
+          //     urlEnabled: true,
+          //     uploadEnabled: true,
+          //     uploadCallback: uploadImageCallBack, 
+          //     previewImage: true,
+          //     alt: { present: false, mandatory: false } 
+          //   },
+          // }}
         />
+        <button 
+          onClick={handleSubmit}
+          type="button" 
+          className="px-8 py-2 bg-blue-500 text-white rounded-lg mt-2">
+            Submit
+        </button>
       </div>
+      {!!formattedData && (
+        <div className="w-10/12 bg-white p-4 mt-6 rounded-lg overflow-x-auto">
+          <pre >
+            {JSON.stringify(formattedData, undefined, 2)}
+          </pre>
+        </div>
+      )}
     </div>
   )
 }
